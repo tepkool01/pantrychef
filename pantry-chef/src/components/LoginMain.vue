ae8264da3312f15da89f5822db10d06722b9459c
Michael Young <michael.young1@ge.com>
2020-01-30 14:21:02 -0800

    simple adding new login page

---
+++ pantry-chef/src/components/LoginMain.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="login-main">
+    <form>
+      <label>
+        Username
+        <input type="text" name="username" />
+      </label>
+      <label>
+        Password
+        <input type="password" name="password" />
+      </label>
+      <button>Login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoginMain"
+};
+</script>
+
+<style scoped></style>
---
+++ pantry-chef/src/views/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="login">
+    <h1>Login</h1>
+    <login-main></login-main>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import LoginMain from "@/components/LoginMain.vue";
+
+export default {
+  name: "login",
+  components: {
+    LoginMain
+  }
+};
+</script>
