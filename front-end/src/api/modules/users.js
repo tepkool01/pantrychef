ccfeccf3f91280615846d9f65ff28779c09dba23
Michael Young <mvy1221@gmail.com>
2020-02-10 15:59:23 -0800

    Update users.js

--- front-end/src/api/modules/users.js
+++ front-end/src/api/modules/users.js
@@ -3,7 +3,7 @@ export default {
 		// Todo: make API call to users
 		return [{ name: 'TestProfile 1' }, { name: 'TestProfile 1' }]
 	},
-
+	// eslint-disable-next-line no-unused-vars
 	createProfile(profile) {
 		return Promise.resolve()
 	}
