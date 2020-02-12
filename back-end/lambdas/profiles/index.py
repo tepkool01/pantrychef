e55b0f75d7a9a2416ab3a07b68129d9a5015e756
Michael Young <mvy1221@gmail.com>
2020-02-11 20:58:59 -0800

    adding in correct response headers; Now we have a full data round trip!

--- back-end/lambdas/profiles/index.py
+++ back-end/lambdas/profiles/index.py
@@ -8,5 +8,9 @@ def lambda_handler(event, context):
 
   return {
       'statusCode': 200,
+      'headers': {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*"
+      },
       'body': json.dumps(result)
   }
