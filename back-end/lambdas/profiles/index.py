2ff033f20f3e68b39cbcc5380f78e748afe618df
Michael Young <mvy1221@gmail.com>
2020-02-11 20:48:46 -0800

    fixing json

--- back-end/lambdas/profiles/index.py
+++ back-end/lambdas/profiles/index.py
@@ -4,7 +4,7 @@ def lambda_handler(event, context):
   print("Hello World 5")
 
   # Retrieve database content
-  result = [{name: "Test Profile 1"}, {name: "Test Profile 2"}]
+  result = [{"name": "Test Profile 1"}, {"name": "Test Profile 2"}]
 
   return {
       'statusCode': 200,
