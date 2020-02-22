ccc9326c1f5348df6508a51a815e91d59dd4d66c
Michael Young <mvy1221@gmail.com>
2020-02-22 10:28:37 -0800

    using sync only test

--- back-end/lambdas/profiles/index.py
+++ back-end/lambdas/profiles/index.py
@@ -1,6 +1,7 @@
 import json
 import os
 from database import DB
+import cognitojwt
 
 # Everything outside of the handler is 'cached' on the virtual machine, connections should be here
 # Initialize the DB connect
@@ -8,10 +9,27 @@ db = DB(database_name=os.environ['DB_NAME'], cluster_arn=os.environ['RDS_ARN'], 
 
 
 def lambda_handler(event, context):
-
     print(event)
     print(context)
     result = {}
+
+    # Grab Bearer token, without the bearer, and it becomes a normal JWT we can decode
+    id_token = event['headers']['Authorization'].replace('Bearer ', '')
+    print(id_token)
+    REGION = 'us-east-1'  # todo: become part of environ
+    USERPOOL_ID = ' us-east-1_895IYJN1N'
+    APP_CLIENT_ID = '1f4k0ktrcbthkq7foan121c9sq'
+
+    # Sync mode
+    verified_claims: dict = await cognitojwt.decode_async(
+        id_token,
+        REGION,
+        USERPOOL_ID,
+        app_client_id=APP_CLIENT_ID,  # Optional
+        testmode=True  # Disable token expiration check for testing purposes
+    )
+    print("Claim!!!")
+    print(verified_claims)
 
     if event['resource'] == '/profiles':
         if event['httpMethod'] == 'GET':
