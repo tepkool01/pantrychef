8617ba0ef0abe42b1e0acfa87ef3c9e6e069e745
Michael Young <mvy1221@gmail.com>
2020-02-22 15:07:35 -0800

    New user now adds to the database

--- back-end/lambdas/new-user/index.py
+++ back-end/lambdas/new-user/index.py
@@ -1,18 +1,44 @@
 ###
 # This is a TRIGGER, not an API Gateway compatible lambda; used for when a person confirms their email
 ###
+import os
 from database import DB
+
+db = DB(database_name=os.environ['DB_NAME'], cluster_arn=os.environ['RDS_ARN'], secret_arn=os.environ['Secrets_ARN'])
 
 
 def lambda_handler(event, context):
-    print("Event incoming")
     print(event)
 
+    # This is the event that happens/triggers after they confirm their email address
     if event['triggerSource'] == 'PostConfirmation_ConfirmSignUp':
-        print("Username:", event['userName'])
-        print("Sub:", event['request']['userAttributes']['sub'])
-        print("Email:", event['request']['userAttributes']['sub'])
-        # todo: add to database
+        try:
+            # Doing a replace for testing, but could be an INSERT
+            db.execute(
+                sql="REPLACE INTO `User` SET Username=:username, CognitoID=:sub, IsValidated=:validated",
+                parameters=[
+                    {
+                        'name': 'username',
+                        'value': {
+                            'stringValue': event['userName']
+                        }
+                    },
+                    {
+                        'name': 'sub',
+                        'value': {
+                            'stringValue': event['request']['userAttributes']['sub']
+                        }
+                    },
+                    {
+                        'name': 'validated',
+                        'value': {
+                            'booleanValues': True
+                        }
+                    }
+                ]
+            )
+        except Exception as e:
+            # Todo: maybe retry?
+            print("ERROR!!! Could not add user!", str(e))
 
     return event
-
