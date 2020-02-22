c35ea9f266fe6f49edac3beb8cb809e81b6c4490
Michael Young <mvy1221@gmail.com>
2020-02-21 20:34:45 -0800

    saving work; working RDS connection with executable statement

--- back-end/lambdas/profiles/database.py
+++ back-end/lambdas/profiles/database.py
@@ -3,8 +3,22 @@ import base64
 from botocore.exceptions import ClientError
 
 
-def get_secret():
+# Todo: make this a class
+def do_stuff():
+    client = boto3.client('rds-data')
+    response = client.execute_statement(
+        database="pantryDB",
+        resourceArn="arn:aws:rds:us-east-1:878527066650:cluster:pantrychef",
+        secretArn="arn:aws:secretsmanager:us-east-1:878527066650:secret:pantrychefdb-OKx5ZI",
+        sql="SELECT * FROM `Ingredient`"
+    )
+    print("Come response")
+    print(response)
 
+
+# https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/rds-data.html
+
+def get_secret():
     secret_name = "pantrychefdb"
     region_name = "us-east-1"
 
@@ -19,32 +33,22 @@ def get_secret():
         get_secret_value_response = client.get_secret_value(
             SecretId=secret_name
         )
-        print(get_secret_value_response)
     except ClientError as e:
         if e.response['Error']['Code'] == 'DecryptionFailureException':
-            # Secrets Manager can't decrypt the protected secret text using the provided KMS key.
-            # Deal with the exception here, and/or rethrow at your discretion.
             raise e
         elif e.response['Error']['Code'] == 'InternalServiceErrorException':
-            # An error occurred on the server side.
-            # Deal with the exception here, and/or rethrow at your discretion.
             raise e
         elif e.response['Error']['Code'] == 'InvalidParameterException':
-            # You provided an invalid value for a parameter.
-            # Deal with the exception here, and/or rethrow at your discretion.
             raise e
         elif e.response['Error']['Code'] == 'InvalidRequestException':
-            # You provided a parameter value that is not valid for the current state of the resource.
-            # Deal with the exception here, and/or rethrow at your discretion.
             raise e
         elif e.response['Error']['Code'] == 'ResourceNotFoundException':
-            # We can't find the resource that you asked for.
-            # Deal with the exception here, and/or rethrow at your discretion.
             raise e
+    else:
+        if 'SecretString' in get_secret_value_response:
+            secret = get_secret_value_response['SecretString']
         else:
-            # Decrypts secret using the associated KMS CMK.
-            # Depending on whether the secret is a string or binary, one of these fields will be populated.
-            if 'SecretString' in get_secret_value_response:
-                secret = get_secret_value_response['SecretString']
-            else:
-                decoded_binary_secret = base64.b64decode(get_secret_value_response['SecretBinary'])
+            secret = base64.b64decode(get_secret_value_response['SecretBinary'])
+    return secret
+
+
--- back-end/lambdas/profiles/index.py
+++ back-end/lambdas/profiles/index.py
@@ -1,15 +1,15 @@
 import json
-from database import get_secret
+from database import get_secret, do_stuff
 
 def lambda_handler(event, context):
 
     print(event)
-    print(context)
+    #print(context)
     print("Hello World 5")
 
-    get_secret()
+    print(get_secret())
 
-
+    print(do_stuff())
     # Retrieve database content
     result = [{"name": "Test Profile 1"}, {"name": "Test Profile 2"}]
 
