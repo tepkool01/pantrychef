d02e8f076c95c94cb22e61b3f7d9f20d7ab6f058
Michael Young <mvy1221@gmail.com>
2020-02-22 12:29:12 -0800

    Adding new-user trigger

---
+++ back-end/lambdas/new-user/index.py
@@ -0,0 +1,17 @@
+###
+# This is a TRIGGER, not an API Gateway compatible lambda; used for when a person confirms their email
+###
+from database import DB
+
+
+def lambda_handler(event, context):
+    print("Event incoming")
+    print(event)
+
+    if event['triggerSource'] == 'PostConfirmation_ConfirmSignUp':
+        print("Username:", event['userName'])
+        print("Sub:", event['request']['userAttributes']['sub'])
+        print("Email:", event['request']['userAttributes']['sub'])
+        # todo: add to database
+
+    return event
