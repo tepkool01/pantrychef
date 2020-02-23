797eb132d5a5c5db51fa7345bcd10011f6e720aa
Michael Young <mvy1221@gmail.com>
2020-02-22 16:46:44 -0800

    first attempt at saving a profile

--- back-end/lambdas/profiles/database.py
+++ back-end/lambdas/profiles/database.py
@@ -2,7 +2,7 @@ import boto3
 
 # For more information regarding the underlying boto calls and how to expand this class, go here:
 # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/rds-data.html
-
+# Todo: potentially place this into a lambda layer to be shared across all lambdas
 
 """ Utility class for accessing the Database and running executable statements """
 
@@ -12,17 +12,38 @@ class DB:
     """Initialize the variables that will be used in all the executable statements"""
     def __init__(self, database_name, cluster_arn, secret_arn):
         self.client = boto3.client('rds-data')
-        self.database_name = database_name
-        self.cluster_arn = cluster_arn
-        self.secret_arn = secret_arn
+        self.database = database_name
+        self.db_params = {
+            'resourceArn': cluster_arn,
+            'secretArn': secret_arn
+        }
 
     """Executes a SQL Statement"""
-    def execute(self, sql, parameters):
-        return self.client.execute_statement(
-            database=self.database_name,
-            resourceArn=self.cluster_arn,
-            secretArn=self.secret_arn,
-            sql=sql,
-            parameters=parameters
-        )
+    def execute(self, sql, parameters, transaction_id=''):
+        required_params = {
+            'database': self.database,
+            'sql': sql,
+            'parameters': parameters
+        }
+        if len(transaction_id) > 0:
+            return self.client.execute_statement(
+                **self.db_params,
+                **required_params,
+                **{'transaction_id': transaction_id}
+            )
+        else:
+            return self.client.execute_statement(
+                **self.db_params,
+                **required_params
+            )
+
+    def begin_transaction(self):
+        result = self.client.begin_transaction(**self.db_params, **{'database': self.database})
+        return result['transactionId']
+
+    def commit_transaction(self, transaction_id):
+        return self.client.commit_transaction(**self.db_params, **{'transactionId': transaction_id})
+
+    def rollback_transaction(self, transaction_id):
+        return self.client.commit_transaction(**self.db_params, **{'transactionId': transaction_id})
 
--- back-end/lambdas/profiles/index.py
+++ back-end/lambdas/profiles/index.py
@@ -10,19 +10,36 @@ db = DB(database_name=os.environ['DB_NAME'], cluster_arn=os.environ['RDS_ARN'], 
 
 def lambda_handler(event, context):
     print(event)
-    print(context)
-    result = {}
 
+    # Safe defaults for return values
+    result = {}
+    status_code = 200  # todo: make 201
+
+    # Incoming payload from user
+    payload = json.loads(event.body)
+    print(payload)
+
+    ###
     # Grab data about person invoking the request
+    ###
     ident = Identity(event)
     claim = ident.get_claim()
-    print(claim)
     if 'sub' in claim:
         print("User is actively logged in, their user ID (cognitoId) is:", claim['sub'])
     else:
-        # todo
         print("User token is expired, corrupted, we should exit/aka return out of the lambda early")
+        return {
+            'statusCode': 403,
+            'headers': {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*"
+            },
+            'body': json.dumps(result)
+        }
 
+    ###
+    # Actual Routing
+    ###
     if event['resource'] == '/profiles':
         if event['httpMethod'] == 'GET':
             # Retrieve all profiles
@@ -32,16 +49,47 @@ def lambda_handler(event, context):
                     {
                         'name': 'cognitoId',
                         'value': {
-                            'stringValue': ''
+                            'stringValue': claim['sub']
                         }
                     }
                 ]
             )
             # todo: parse info
         elif event['httpMethod'] == 'POST':
-            result = {}
             # Create a profile
             print("Create a profile")
+            transaction_id = db.begin_transaction()
+            try:
+                list1 = db.execute(
+                    sql="INSERT INTO `IngredientList` (ListType) VALUES(:stuff)",
+                    parameters=[{'name': 'stuff', 'value': {'stringValue': 'WhatGoesHere'}}],
+                    transaction_id=transaction_id
+                )
+                print(list1)
+                list2 = db.execute(
+                    sql="INSERT INTO `IngredientList` (ListType) VALUES(:stuff)",
+                    parameters=[{'name': 'stuff', 'value': {'stringValue': 'WhatGoesHere'}}],
+                    transaction_id=transaction_id
+                )
+                print(list2)
+                profile = db.execute(
+                    sql="INSERT INTO `UserProfile` (UserID, DietType, PantryList, ShoppingList) VALUES(:userId, :dietType, :pantryList, :shoppingList)",
+                    parameters=[
+                        {'name': 'userId', 'value': {'longValue': int()}},
+                        {'name': 'dietType', 'value': {'longValue': int(1)}},
+                        {'name': 'pantryList', 'value': {'longValue': int(1)}},
+                        {'name': 'shoppingList', 'value': {'longValue': int(2)}},
+                    ],
+                    transaction_id=transaction_id
+                )
+                print(profile)
+                db.commit_transaction(transaction_id)
+                result = profile
+            except Exception as e:
+                db.rollback_transaction(transaction_id)
+                status_code = 500
+                result = {'errorMessage': 'Could not save the profile.'}
+                print(str(e))
 
     elif event['resource'] == '/profiles/{profileId}':
         result = {}
@@ -49,7 +97,7 @@ def lambda_handler(event, context):
         print("profile ID:", event['pathParameters']['profileId'])
 
     return {
-        'statusCode': 200,
+        'statusCode': status_code,
         'headers': {
             "Content-Type": "application/json",
             "Access-Control-Allow-Origin": "*"
