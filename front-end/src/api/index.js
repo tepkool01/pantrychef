32b261b4e4a81443b91f3c25e9135c0aedb73bae
Nathan Fox <knafox@gmail.com>
2020-03-07 15:16:44 -0600

    Removing Ingredient List changes

--- front-end/src/api/index.js
+++ front-end/src/api/index.js
@@ -1,13 +1,11 @@
 import profile from './modules/profile.js'
 import users from './modules/users.js'
 import ingredients from "./modules/ingredients"
-import ingredientList from "./modules/ingredientList"
 import recipe from './modules/recipe.js'
 
 export default {
 	users,
 	profile,
 	ingredients,
-	ingredientList,
 	recipe
 }
