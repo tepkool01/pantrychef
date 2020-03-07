23518d3a93c4b16b43b7616b80b30b0dda682e02
Eric Cortez <nabi2Cortez@gmail.com>
2020-03-06 19:03:57 -0600

    added ViewRecipe file and function for button in recipes page

---
+++ front-end/src/views/ViewRecipe.vue
@@ -0,0 +1,33 @@
+<template>
+
+    <div>
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="mt-4">View Recipe</h1>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<script>
+    export default{
+        name:"Todos",
+        props: ["Todos"]
+    }
+
+</script>
+
+<style scoped>
+    .settings--section-label {
+        font-size: 10px;
+        font-weight: 700;
+        letter-spacing: 0.5px;
+        line-height: 12px;
+        text-transform: uppercase;
+        border-bottom: 2px solid #edeff1;
+        color: #7c7c7c;
+        margin-bottom: 32px;
+        padding-bottom: 6px;
+    }
+</style>
