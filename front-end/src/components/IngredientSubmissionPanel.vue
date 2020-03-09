5c88d607dd36b423f1e71b68ccc22395422bfcfb
Michael Loredo <mjloredo@gmail.com>
2020-03-08 19:32:42 -0500

    PAN-50 creating Ingredient Add component.

---
+++ front-end/src/components/IngredientSubmissionPanel.vue
@@ -0,0 +1,89 @@
+<template>
+    <div class="card m-4" style="width: 30%">
+        <div class="card-header">
+            <h5 class="card-title">
+                Add Ingredient
+            </h5>
+        </div>
+        <div class="card-body my-2" style="width: 18rem;">
+            <AutoComplete :componentTitle="Title1" :suggestionsMstr="suggestionsMstr" :objectAttribute="Attr1" :toolTip="Tip1" ></autoComplete>
+            <br>
+
+            <AutoComplete :componentTitle="Title2" :suggestionsMstr="unitsOfMeasure" :objectAttribute="Attr2" :toolTip="Tip2" ></autoComplete>
+            <br>
+
+
+            <label v-b-tooltip.hover.left v-bind:title="Tip3" 
+            for="lfname">{{Title3}}:</label> 
+            <input type="number" step=0.25 min=0.0 max=999.9 value=0.0 pattern="^\d*(\.\d{0,2})?$" class="bg-gray-300 px-4 py-2" autocomplete="off" oninput="this.value = Math.abs(this.value)" v-model="Value3">
+            <br>
+
+            <label v-b-tooltip.hover.left v-bind:title="Tip4" 
+            for="lfname">{{Title4}}:</label> 
+            <br>
+
+            <input type="date" id="start" name="trip-start" v-bind:value="currentDate" v-bind:min="currentDate">
+            <br><br><br>
+            <b-button variant="outline-secondary" class="ml-3"
+			>Add</b-button
+		>
+        </div>
+    </div>
+</template>
+
+<script>
+import AutoComplete from '@/components/AutoComplete.vue'
+
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+	name: 'ingredients',
+     props: {
+          suggestionsMstr: {
+            type: Array,
+            required: true
+          }
+      },
+	components: {
+		AutoComplete
+	},
+	data: function () {
+		return {
+			unitsOfMeasure: [ 
+				{"name": "kilogram",
+				 "abbreviation": "kg"
+				},
+				{"name": "gram",
+				 "abbreviation": "g"
+				},
+				{"name": "milligram",
+				 "abbreviation": "mg"
+				},
+				{"name": "pound",
+				"abbreviation":"lb"
+				},
+				{"name":"liter",
+				  "abbreviation":"L"
+				},
+				{"name":"milliliter",
+				"abbreviation":"mL"
+				}],
+			Title1: 'Ingredient',
+			Attr1: 'ingredient_name',
+			Tip1: 'Choose ingredient to add to your pantry.',
+			Title2: 'Unit',
+			Attr2: 'abbreviation',
+			Tip2: 'Choose the unit of measure for your ingredient.',
+            Value3: 0.0,
+            Value3Previous:0.0,
+            Title3: 'Amount',
+            Tip3: 'Choose the amount for your ingredient.',
+            Title4: 'Expiration Date',
+            Tip4: 'Choose ingredient expiration date.',
+            currentDate: new Date().toISOString().substr(0, 10)
+		}
+	}
+}
+</script>
+
+<style scoped></style>
--- front-end/src/views/Ingredients.vue
+++ front-end/src/views/Ingredients.vue
@@ -1,48 +1,33 @@
 <template>
 	<div>
 		<div class="row">
-			<div class="col-lg-12">
-				<h1 class="mt-4">Display Ingredients</h1>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-lg-10 offset-1">
-				<div class="card-deck">
-					<div class="card m-4">
-						<div class="card-header">
-							<h5 class="card-title">Add new Ingredients</h5>
-						</div>
-						<div class="card-body my-2">
-							<AutoComplete :componentTitle="Title1" :suggestionsMstr="ingredients" :objectAttribute="Attr1" :toolTip="Tip1" ></autoComplete>
-							<AutoComplete :componentTitle="Title2" :suggestionsMstr="unitsOfMeasure" :objectAttribute="Attr2" :toolTip="Tip2" ></autoComplete>
-		
-				
-						</div>
+	
+					<IngredientSubmissionPanel :suggestionsMstr="ingredients" ></IngredientSubmissionPanel>
 
-					</div>
-
-					<div class="card m-4">
+					<div class="card m-4 text-center" style="width:50%">
 						<div class="card-header">
 							<h5 class="card-title">
-								Ingredients in Pantry
+								Ingredients In Pantry
 							</h5>
 						</div>
-						<div class="card-body my-2">
+						<div class="card-body m-4 text-center" style="width: 50%">
 							<div v-for="i in ingredients" v-bind:key="i.name">
 								<ingredient :ingredient="i" :key="i.id"></ingredient>
 							</div>
-							</div>
 						</div>
 					</div>
-				</div>
-			</div>
+
 		</div>
 	</div>
 </template>
 
+
+
+
+
 <script>
 import Ingredient from '@/components/Ingredient.vue'
-import AutoComplete from '@/components/AutoComplete.vue'
+import IngredientSubmissionPanel from '@/components/IngredientSubmissionPanel.vue'
 
 import { mapGetters, mapActions } from 'vuex'
 
@@ -54,8 +39,8 @@ export default {
 		})
 	},
 	components: {
-		Ingredient,
-		AutoComplete
+		IngredientSubmissionPanel,
+		Ingredient
 	},
 	methods: {
 		...mapActions('ingredients', {
@@ -66,35 +51,6 @@ export default {
 	created() {
 		this.getIngredients()
 		this.$emit('title', 'Pantry')
-	},
-	data: function () {
-		return {
-			unitsOfMeasure: [ 
-				{"name": "kilogram",
-				 "abbreviation": "kg"
-				},
-				{"name": "gram",
-				 "abbreviation": "g"
-				},
-				{"name": "milligram",
-				 "abbreviation": "mg"
-				},
-				{"name": "pound",
-				"abbreviation":"lb"
-				},
-				{"name":"liter",
-				  "abbreviation":"L"
-				},
-				{"name":"milliliter",
-				"abbreviation":"mL"
-				}],
-			Title1: 'Ingredient',
-			Attr1: 'ingredient_name',
-			Tip1: 'Choose ingredient to add to your pantry.',
-			Title2: 'Unit',
-			Attr2: 'abbreviation',
-			Tip2: 'Choose the unit of measure for your ingredient.'
-		}
 	}
 }
 </script>
