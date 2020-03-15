25c771ec82ce713f03c939882d70089a2330f15c
Michael Young <mvy1221@gmail.com>
2020-03-14 19:41:56 -0700

    Updating default for CookTime

--- infrastructure/database/dbschemacreation.sql
+++ infrastructure/database/dbschemacreation.sql
@@ -57,7 +57,7 @@ CREATE TABLE ShoppingListItem (
 CREATE TABLE Recipe (
     ID INT NOT NULL AUTO_INCREMENT,
     RecipeName VARCHAR(255),
-    CookTime INT,
+    CookTime INT DEFAULT 0,
     DietType VARCHAR(10),
     IngredientCount INT DEFAULT 0
     PRIMARY KEY (ID)
