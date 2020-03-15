c1de0c3a5f440f3bee3b135b585903485b1fdbeb
Michael Young <mvy1221@gmail.com>
2020-03-15 16:36:48 -0700

    add ingredient now working

--- front-end/src/api/modules/pantry.js
+++ front-end/src/api/modules/pantry.js
@@ -5,9 +5,6 @@ export default {
 		return axios.get('/pantry/' + profile)
 	},
 	addIngredient(ingredient_id, profile_id) {
-		console.log("Addinggggg")
-		console.log(ingredient_id)
-		console.log(profile_id)
 		return axios.put('/pantry/' + profile_id + '/ingredients/' + ingredient_id)
 	},
 	deleteIngredient(ingredient_id, profile_id) {
