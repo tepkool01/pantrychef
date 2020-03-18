71619a349f3244dc04b489390bdc5b4f0c12a00d
Eric Cortez <nabi2Cortez@gmail.com>
2020-03-17 23:34:46 -0500

    <template>     <div>         <div class="row">             <div class="card m-4 text-center" style="...">                 <div class="card-header">                     <h1 class="card-title">                         View Recipe                     </h1>                     <p>Retrieve recipe with ID:{{ $route.params.id }}</p>                     <b-icon-star @click="addFavoriteRecipe($route.params.id) " class="star-icon">Add Fav recipe to                         pantry                     </b-icon-star>
                    </div>
                </div>
                <div class="view-recipe--view">
                    <!-- <button @change="true" class="btn btn-sm btn-primary">Add Favorite Recipe to Pantry</button>-->
    
                </div>
            </div>
        </div>
    </template>
    
    <script>
        export default {
            name: 'RecipeCard',
            methods: {
                addFavoriteRecipe(id) {
                    // todo
                    console.log(id)
                }
            }
        }
    
    </script>
    
    <style scoped>
        .star-icon {
            float: left
        }
    </style>

--- front-end/src/views/ViewRecipe.vue
+++ front-end/src/views/ViewRecipe.vue
@@ -1,30 +1,40 @@
 <template>
-
-    <div>
-        <div class="row">
-            <div class="col-lg-12">
-                <h1 class="mt-4">View Recipe</h1>
-                <p>Retrieve recipe with ID:{{ $route.params.id }}</p>
-            </div>
-        </div>
-    </div>
+	<div>
+		<div class="row">
+			<div class="card m-4 text-center" style="...">
+				<div class="card-header">
+					<h1 class="card-title">
+						View Recipe
+					</h1>
+					<p>Retrieve recipe with ID:{{ $route.params.id }}</p>
+					<b-icon-star
+						@click="addFavoriteRecipe($route.params.id)"
+						class="star-icon"
+						>Add Fav recipe to pantry
+					</b-icon-star>
+				</div>
+			</div>
+			<div class="view-recipe--view">
+				<!-- <button @change="true" class="btn btn-sm btn-primary">Add Favorite Recipe to Pantry</button>-->
+			</div>
+		</div>
+	</div>
 </template>
 
-
 <script>
-
+export default {
+	name: 'RecipeCard',
+	methods: {
+		addFavoriteRecipe(id) {
+			// todo
+			console.log(id)
+		}
+	}
+}
 </script>
 
 <style scoped>
-    .settings--section-label {
-        font-size: 10px;
-        font-weight: 700;
-        letter-spacing: 0.5px;
-        line-height: 12px;
-        text-transform: uppercase;
-        border-bottom: 2px solid #edeff1;
-        color: #7c7c7c;
-        margin-bottom: 32px;
-        padding-bottom: 6px;
-    }
+.star-icon {
+	float: left;
+}
 </style>
