80411bee8c6d50a17dba8761265e7a27acad7580
Michael Young <mvy1221@gmail.com>
2020-03-18 19:50:33 -0700

    api keys; new tracker

--- utilities/spoonacular_scraper.py
+++ utilities/spoonacular_scraper.py
@@ -10,10 +10,13 @@ today = date.today()
 date_filename = today.strftime("%Y%m%d")
 
 # CHANGE THESE NUMBERS!!!
-start = 130
-end = 260  # max for free tier is 150 a day, so I incremented by 130, to be safe
+start = 400
+end = 540  # max for free tier is 150 a day, so I incremented by 130, to be safe
 
-api_key = 'c1de684c34644eb29f056d11a35d94b0'  # MY's key
+#api_key = 'c1de684c34644eb29f056d11a35d94b0'  # MY's key
+# api_key = '43bb834225f844749aefe3a2a870add5' # MR's key
+# api_key = '5275184a86f94466b97dddb31f766d7c' # Eric's key
+api_key = 'c9a75a2d0140472981d0318f318d9481' # MR2's key
 
 # Grabs 10 entries at a time
 for i in range(start, end, 1):
