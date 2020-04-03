2ef8c06c944a8280175d75bfb864727074d2c0a8
Nathan Fox <nathan.fox@ihsmarkit.com>
2020-04-03 12:43:59 -0500

    Adding basic test.

---
+++ back-end/lambdas/ingredients/test_ingredients.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class IngredientTest(unittest.TestCase):
+
+    def test_Test1(self):
+        self.assertEqual(1, 1)
+
+    def test_Test2(self):
+        self.assertEqual(2, 2)
