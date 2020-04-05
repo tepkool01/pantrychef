d801d3469699f961c6c6fe161a91c1f33180875b
Michael Young <mvy1221@gmail.com>
2020-04-05 10:48:16 -0700

    adding data to View Recipe view

--- front-end/src/views/ViewRecipe.vue
+++ front-end/src/views/ViewRecipe.vue
@@ -3,10 +3,11 @@
 		<div class="row">
 			<div class="card m-4 text-center">
 				<div class="card-header">
+                    <img v-if="recipe.img_url" :src="'img/recipes/' + recipe.img_url" />
 					<h1 class="card-title">
-						View Recipe
+						{{ recipe.name }}
 					</h1>
-					<p>Retrieve recipe with ID:{{ $route.params.id }}</p>
+					<p v-html="recipe.summary"></p>
 					<b-icon-star
 						@click="addFavoriteRecipe($route.params.id)"
 						class="star-icon"
@@ -15,18 +16,55 @@
 				</div>
 			</div>
 			<div class="view-recipe--view">
-				<!-- <button @change="true" class="btn btn-sm btn-primary">Add Favorite Recipe to Pantry</button>-->
+                <p>Cook Time: {{ recipe.cook_time }}</p>
+                <p>Servings: {{ recipe.servings }}</p>
+                <p>Health Score: {{ recipe.health_score }}</p>
+                <p>Weight Watcher Points: {{ recipe.weight_watcher_points }}</p>
+                <p>Vegetarian: {{ recipe.vegetarian }}</p>
+                <p>Vegan: {{ recipe.vegan }}</p>
+                <p>Gluten Free: {{ recipe.gluten_free }}</p>
+                <p>Dairy Free: {{ recipe.dairy_free }}</p>
+                <p>Healthy: {{ recipe.healthy }}</p>
+                <p>Sustainable: {{ recipe.sustainable }}</p>
+                <p>Ingredients</p>
+                <ul>
+                    <li v-for="ingredient in recipe.ingredients" :key="ingredient.id">{{ ingredient.name }}</li>
+                </ul>
+                <p>Directions</p>
+                <ol>
+                    <li v-for="direction in recipe.directions" :key="direction.order">{{ direction.direction }}</li>
+                </ol>
 			</div>
 		</div>
 	</div>
 </template>
 
 <script>
-import { mapActions } from 'vuex';
 import api from '../api';
 
 export default {
 	name: 'RecipeCard',
+    data() {
+	    return {
+	    	recipe: {
+				name: "",
+				cook_time: 0,
+				img_url: "",
+				servings: 0,
+				summary: "",
+				health_score: 4,
+                weight_watcher_points: 2,
+                vegetarian: false,
+                vegan: false,
+                gluten_free: false,
+                dairy_free: false,
+                healthy: false,
+                sustainable: false,
+                directions: [],
+                ingredients: [],
+            }
+        }
+    },
 	methods: {
 		addFavoriteRecipe(id) {
 			// todo
@@ -34,10 +72,10 @@ export default {
 		}
 	},
     created() {
+		// Don't need to hold state, seed recipe into data direct
 		api.recipe.getRecipe(this.$route.params.id).then(result => {
-			console.log(result);
+			this.recipe = result.data
         });
-        console.log("CREATE");
     }
 }
 </script>
@@ -46,7 +84,7 @@ export default {
 .star-icon {
 	float: left;
 }
-    .test {
-        background-color: #2c3e50;
-    }
+.test {
+    background-color: #2c3e50;
+}
 </style>
