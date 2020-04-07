5688d15ffb33c887685acb19a244b83c5084a88f
tepkool01 <mvy1221@gmail.com>
2020-04-06 21:12:34 -0700

    View recipe UI

---
+++ front-end/src/components/modals/ViewRecipe.vue
@@ -0,0 +1,15 @@
+<template>
+	<b-modal id="view-recipe" title="View Recipe">
+		<div>
+			Hey
+		</div>
+	</b-modal>
+</template>
+
+<script>
+export default {
+	name: 'ViewRecipe',
+};
+</script>
+
+<style scoped></style>
