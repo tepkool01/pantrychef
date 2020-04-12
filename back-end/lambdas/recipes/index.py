8cdde95c6886164cb2b6478b5d9c70599ab1ee0d
Michael Young <mvy1221@gmail.com>
2020-04-12 14:22:14 -0700

    working search backend; sql injection issue to look into

--- back-end/lambdas/recipes/index.py
+++ back-end/lambdas/recipes/index.py
@@ -34,33 +34,96 @@ def lambda_handler(event, context):
         limit = 25
         offset = 0
         if event['httpMethod'] == 'GET':
-
+            # Grab active profile to retrieve shopping and pantry list
             active_profile = db.execute(
                 sql="SELECT ID FROM `UserProfile` WHERE UserID=:userId AND IsActive=1 LIMIT 1",
                 parameters=[{'name': 'userId', 'value': {'longValue': int(u.get_id())}}]
             )
-            pantry_item_list = db.execute(
-                sql="SELECT IngredientID FROM `IngredientListItem` WHERE UserProfile=:ProfileID",
-                parameters=[{'name': 'ProfileID', 'value': {'longValue': int(active_profile['records'][0][0]['longValue'])}}]
-            )
 
-            # Ranking logic
-            if len(pantry_item_list['records']) > 0:
-                # Grab user's ingredients they have in their pantry, and place them into a MySQL readable format
-                ingredient_ids = []
+            # Grab all ingredient IDs they want to include in the search
+            ingredient_ids = []
+            if event['queryStringParameters']['pantry_list'] == 'true':
+                # include pantry list in search criteria / ranking
+                pantry_item_list = db.execute(
+                    sql="SELECT IngredientID FROM `IngredientListItem` WHERE UserProfile=:ProfileID",
+                    parameters=[{'name': 'ProfileID',
+                                 'value': {'longValue': int(active_profile['records'][0][0]['longValue'])}}]
+                )
                 for ingredient_id in pantry_item_list['records']:
                     ingredient_ids.append(ingredient_id[0]['longValue'])
+
+            if event['queryStringParameters']['shopping_list'] == 'true':
+                # include shopping list in search criteria
+                shopping_item_list = db.execute(
+                    sql="SELECT IngredientID FROM `ShoppingListItem` WHERE UserProfile=:ProfileID",
+                    parameters=[{'name': 'ProfileID',
+                                 'value': {'longValue': int(active_profile['records'][0][0]['longValue'])}}]
+                )
+                for ingredient_id in shopping_item_list['records']:
+                    ingredient_ids.append(ingredient_id[0]['longValue'])
+
+            ##
+            # Slightly hacky conditional statements that could be refactored DRY
+            ##
+
+            # Search by ingredients AND search name
+            if len(ingredient_ids) > 0 and len(event['queryStringParameters']['search']) > 0:
+                print("Ingredient and search name")
                 my_ingredient_ids = ','.join(map(str, ingredient_ids))
                 print(my_ingredient_ids)
+                # TODO: Insecure
 
                 recipes = db.execute(
                     sql="SELECT RecipeID, RecipeName, CookTime, Servings, IngredientCount, count(*) as grp_count, round((count(*) / IngredientCount), 2) as pct_match, ImgURL, Summary, HealthScore, WeightWatcherPoints, Vegetarian, Vegan, GlutenFree, DairyFree, Healthy, Sustainable \
                         FROM `RecipeListItem` ri \
                         LEFT JOIN Recipe r \
                         ON r.ID=ri.RecipeID \
-                        WHERE ri.IngredientID IN (" + my_ingredient_ids + ") \
+                        WHERE ri.IngredientID IN (" + my_ingredient_ids + ") AND RecipeName LIKE '%" + event['queryStringParameters']['search'] + "%' \
                         GROUP BY ri.RecipeID \
                         ORDER BY pct_match DESC LIMIT :offset, :limit",
+                    parameters=[
+                        {'name': 'limit', 'value': {'longValue': int(limit)}},
+                        {'name': 'offset', 'value': {'longValue': int(offset)}},
+                        {'name': 'recipe_name', 'value': {'stringValue': str(event['queryStringParameters']['search'])}},
+                    ]
+                )
+
+                result = []
+                for record in recipes['records']:
+                    result.append({
+                        'id': record[0]['longValue'],
+                        'recipe_name': record[1]['stringValue'],
+                        'cook_time': record[2]['longValue'],
+                        'servings': record[3]['longValue'],
+                        'ingredient_count': record[4]['longValue'],
+                        'ingredients_in_pantry': record[5]['longValue'],
+                        'match_percent': record[6]['stringValue'],
+                        'img_url': record[7]['stringValue'],
+                        'summary': record[8]['stringValue'],
+                        'health_score': record[9]['doubleValue'],
+                        'weight_watcher_points': record[10]['longValue'],
+                        'vegetarian': record[11]['booleanValue'],
+                        'vegan': record[12]['booleanValue'],
+                        'gluten_free': record[13]['booleanValue'],
+                        'dairy_free': record[14]['booleanValue'],
+                        'healthy': record[15]['booleanValue'],
+                        'sustainable': record[16]['booleanValue'],
+                    })
+
+            # Search just by ingredients, no search name
+            elif len(ingredient_ids) > 0 and len(event['queryStringParameters']['search']) == 0:
+                print("Ingredient search")
+                my_ingredient_ids = ','.join(map(str, ingredient_ids))
+                print(my_ingredient_ids)
+
+                recipes = db.execute(
+                    sql="SELECT RecipeID, RecipeName, CookTime, Servings, IngredientCount, count(*) as grp_count, round((count(*) / IngredientCount), 2) as pct_match, ImgURL, Summary, HealthScore, WeightWatcherPoints, Vegetarian, Vegan, GlutenFree, DairyFree, Healthy, Sustainable \
+                            FROM `RecipeListItem` ri \
+                            LEFT JOIN Recipe r \
+                            ON r.ID=ri.RecipeID \
+                            WHERE ri.IngredientID IN (" + my_ingredient_ids + ") \
+                            GROUP BY ri.RecipeID \
+                            ORDER BY pct_match DESC LIMIT :offset, :limit",
                     parameters=[
                         {'name': 'limit', 'value': {'longValue': int(limit)}},
                         {'name': 'offset', 'value': {'longValue': int(offset)}},
@@ -89,12 +152,53 @@ def lambda_handler(event, context):
                         'sustainable': record[16]['booleanValue'],
                     })
 
-                # Parse records into python object
-                print("I have stuff in my pantry!")
+            # Search just by search name
+            elif len(ingredient_ids) == 0 and len(event['queryStringParameters']['search']) > 0:
+                print("Search by name")
+                # TODO: This is a security issue, but I can't figure out right now how to get the parameter in
+                recipes = db.execute(
+                    sql="SELECT RecipeID, RecipeName, CookTime, Servings, IngredientCount, count(*) as grp_count, round((count(*) / IngredientCount), 2) as pct_match, ImgURL, Summary, HealthScore, WeightWatcherPoints, Vegetarian, Vegan, GlutenFree, DairyFree, Healthy, Sustainable \
+                            FROM `RecipeListItem` ri \
+                            LEFT JOIN Recipe r \
+                            ON r.ID=ri.RecipeID \
+                            WHERE r.RecipeName LIKE '%" + event['queryStringParameters']['search'] + " %' \
+                            GROUP BY ri.RecipeID \
+                            ORDER BY pct_match DESC LIMIT :offset, :limit",
+                    parameters=[
+                        {'name': 'limit', 'value': {'longValue': int(limit)}},
+                        {'name': 'offset', 'value': {'longValue': int(offset)}},
+                        {'name': 'recipeName', 'value': {'stringValue': str(event['queryStringParameters']['search'])}},
+                    ]
+                )
+
+                result = []
+                for record in recipes['records']:
+                    result.append({
+                        'id': record[0]['longValue'],
+                        'recipe_name': record[1]['stringValue'],
+                        'cook_time': record[2]['longValue'],
+                        'servings': record[3]['longValue'],
+                        'ingredient_count': record[4]['longValue'],
+                        'ingredients_in_pantry': record[5]['longValue'],
+                        'match_percent': record[6]['stringValue'],
+                        'img_url': record[7]['stringValue'],
+                        'summary': record[8]['stringValue'],
+                        'health_score': record[9]['doubleValue'],
+                        'weight_watcher_points': record[10]['longValue'],
+                        'vegetarian': record[11]['booleanValue'],
+                        'vegan': record[12]['booleanValue'],
+                        'gluten_free': record[13]['booleanValue'],
+                        'dairy_free': record[14]['booleanValue'],
+                        'healthy': record[15]['booleanValue'],
+                        'sustainable': record[16]['booleanValue'],
+                    })
+
+            # Empty search
             else:
+                print("Empty search")
                 # They have no items in their pantry, show them everything
                 recipe = db.execute(
-                    sql="SELECT ID, RecipeName, CookTime, ImgURL, Servings, Summary, HealthScore, WeightWatcherPoints, Vegetarian, Vegan, GlutenFree, DairyFree, Healthy, Sustainable FROM Recipe LIMIT :offset, :limit",
+                    sql="SELECT ID, RecipeName, CookTime, ImgURL, Servings, Summary, HealthScore, WeightWatcherPoints, Vegetarian, Vegan, GlutenFree, DairyFree, Healthy, Sustainable, IngredientCount FROM Recipe LIMIT :offset, :limit",
                     parameters=[
                         {'name': 'limit', 'value': {'longValue': int(limit)}},
                         {'name': 'offset', 'value': {'longValue': int(offset)}},
@@ -117,6 +221,8 @@ def lambda_handler(event, context):
                         'dairy_free': record[11]['booleanValue'],
                         'healthy': record[12]['booleanValue'],
                         'sustainable': record[13]['booleanValue'],
+                        'ingredient_count': record[14]['longValue'],
+                        'ingredients_in_pantry': 0,
                     })
     elif event['resource'] == '/recipes/{recipeId}':
         result = {
