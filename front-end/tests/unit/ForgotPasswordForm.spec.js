7f7e4cfaa9a18026e0156715d3fe1f46d3923201
Michael Loredo <mjloredo@gmail.com>
2020-04-12 19:57:40 -0500

    forgot password form test

---
+++ front-end/tests/unit/ForgotPasswordForm.spec.js
@@ -0,0 +1,134 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+import Vuex from 'vuex';
+import ForgotPasswordForm from '../../src/components/ForgotPasswordForm.vue';
+import { BootstrapVue } from "bootstrap-vue";
+import { CognitoUserPool } from 'amazon-cognito-identity-js';
+import store from '../../src/store';
+import users from "../../src/store/modules/users";
+
+let wrapper;
+let actions;
+let mutations;
+let state;
+const localVue = createLocalVue();
+localVue.use(Vuex);
+localVue.use(BootstrapVue);
+
+
+beforeEach(() => {
+    actions = {
+        someAction: jest.fn()
+    };
+    mutations = {
+        someMutation: jest.fn()
+    };
+    state = {
+        key: {}
+    }
+});
+
+afterEach(() => {
+
+});
+
+describe('ForgotPasswordForm', () => {
+    test('ForgotPasswordForm component load default', () => {
+        const wrapper = shallowMount(ForgotPasswordForm, {
+        });
+
+
+        expect(wrapper.vm.user.username).toEqual('');
+        expect(wrapper.vm.validation.errors).toEqual([]);
+        expect(wrapper.vm.validation.username).toBe(false);
+
+        wrapper.destroy();
+    });
+
+    test('ForgotPasswordForm validation', () => {
+        const wrapper = shallowMount(ForgotPasswordForm, {
+        });
+
+        const preventDefault = jest.fn();
+
+        expect(wrapper.vm.user.username).toEqual('');
+        wrapper.find('form').trigger('submit', { preventDefault });
+
+        wrapper.destroy();
+    });
+
+    test('ForgotPasswordForm rejected submission', async () => {
+		wrapper = shallowMount(ForgotPasswordForm, {
+			store: new Vuex.Store({
+				modules: {
+					users: {
+						namespaced: true,
+						state: users.state,
+						actions: {
+							forgotPassword: jest.fn(() => {
+								return new Promise((resolve, reject) => {
+									reject(new Error("some error"))
+								})
+							})
+						},
+						getters: users.getters,
+						mutations: users.mutations
+					},
+				}
+			}),
+			localVue
+		});
+
+        const preventDefault = jest.fn();
+
+        wrapper.vm.user.username='MrBob1234'
+        expect(wrapper.vm.user.username).toEqual('MrBob1234');
+        wrapper.find('form').trigger('submit', { preventDefault });
+
+		expect(CognitoUserPool).toHaveBeenCalledWith({
+			UserPoolId: 'us-east-1_FfJ4ffeia',
+			ClientId: '2lk7bjr0akm1ncuo8i8piqv33g'
+		});
+        wrapper.destroy();
+    });
+
+    test('ForgotPasswordForm approved submission', async () => {
+		wrapper = shallowMount(ForgotPasswordForm, {
+			store: new Vuex.Store({
+				modules: {
+					users: {
+						namespaced: true,
+						state: users.state,
+						actions: {
+							forgotPassword: jest.fn(() => {
+								return new Promise((resolve) => {
+									resolve(1)
+								})
+							})
+						},
+						getters: users.getters,
+						mutations: users.mutations
+					},
+				}
+			}),
+			localVue
+		});
+
+        const preventDefault = jest.fn();
+
+        wrapper.vm.user.username='MrBob1234'
+        expect(wrapper.vm.user.username).toEqual('MrBob1234');
+        wrapper.find('form').trigger('submit', { preventDefault });
+
+		expect(CognitoUserPool).toHaveBeenCalledWith({
+			UserPoolId: 'us-east-1_FfJ4ffeia',
+			ClientId: '2lk7bjr0akm1ncuo8i8piqv33g'
+        });
+        
+        wrapper.destroy();
+    });
+
+
+
+
+
+});
