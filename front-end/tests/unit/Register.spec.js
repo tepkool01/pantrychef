b0abf0ac68f1a7f0bbd5e461729320996c6557ad
Michael_Rodriguez <mmr94@txstate.edu>
2020-04-14 01:36:50 -0500

    Created Register and Settings view tests and have 100% coverage

---
+++ front-end/tests/unit/Register.spec.js
@@ -0,0 +1,51 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+import Vuex from 'vuex';
+import { BootstrapVue } from 'bootstrap-vue';
+import Component from '../../src/views/Register.vue';
+import RegisterForm from '../../src/components/RegisterForm.vue';
+import RegisterSuccess from '../../src/components/RegisterSuccess.vue';
+import store from '../../src/store';
+
+let wrapper;
+
+const localVue = createLocalVue();
+localVue.use(Vuex);
+localVue.use(BootstrapVue);
+
+beforeEach(() => {
+	wrapper = shallowMount(Component, {
+		name: 'register',
+		components: {
+			RegisterForm,
+			RegisterSuccess,
+		},
+		data() {
+			return {
+				showSuccessPage: false,
+			};
+		},
+		mocks: {},
+		stubs: {},
+		methods: {},
+		store,
+		localVue,
+	});
+});
+
+afterEach(() => {
+	wrapper.destroy();
+});
+
+describe('Register', () => {
+	test('renders register component as a failed component', () => {
+		wrapper.vm.showSuccessComponent(false);
+		expect(wrapper.vm.showSuccessPage).toEqual(false);
+	});
+});
+
+describe('Register', () => {
+	test('renders register component as a successful component', () => {
+		wrapper.vm.showSuccessComponent(true);
+		expect(wrapper.vm.showSuccessPage).toEqual(true);
+	});
+});
---
+++ front-end/tests/unit/Settings.spec.js
@@ -0,0 +1,46 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+import Vuex from 'vuex';
+import { BootstrapVue } from 'bootstrap-vue';
+import Component from '../../src/views/Settings.vue';
+import SettingsAccount from '../../src/components/SettingsAccount';
+import SettingsProfiles from '../../src/components/SettingsProfiles';
+import SettingsFood from '../../src/components/SettingsFood';
+import store from "../../src/store";
+
+let wrapper;
+let actions;
+let mutations;
+let state;
+const localVue = createLocalVue();
+localVue.use(Vuex);
+localVue.use(BootstrapVue);
+
+
+beforeEach(() => {
+	wrapper = shallowMount(Component, {
+		name: 'Settings',
+		components: {
+			SettingsFood,
+			SettingsProfiles,
+			SettingsAccount,
+		},
+		mocks: {},
+		stubs: {},
+		methods: {},
+		store,
+		localVue,
+	});
+});
+
+afterEach(() => {
+
+});
+
+describe('Settings', () => {
+	test('renders Settings with the default data', async () => {
+		await wrapper.vm.$nextTick();
+		wrapper.vm.$emit('title', 'Settings');
+		expect(wrapper.emitted().title[0]).toEqual(['Settings']);
+		wrapper.destroy();
+	});
+});
