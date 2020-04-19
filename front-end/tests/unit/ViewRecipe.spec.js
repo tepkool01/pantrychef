103e11454b03f5c360a3d1a610168807f30fc06f
Michael_Rodriguez <mmr94@txstate.edu>
2020-04-18 23:29:20 -0500

    Adding viewRecipe modal test

---
+++ front-end/tests/unit/ViewRecipe.spec.js
@@ -0,0 +1,59 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+import Vuex from 'vuex';
+import { BootstrapVue } from 'bootstrap-vue';
+import Component from '../../src/components/modals/ViewRecipe.vue';
+import store from '../../src/store';
+import router from '../../src/router';
+import api from '../../src/api';
+import axios from "axios";
+import profile from "../../src/store/modules/profile";
+
+let wrapper;
+
+const localVue = createLocalVue();
+localVue.use(Vuex);
+localVue.use(BootstrapVue);
+
+beforeEach(() => {
+	wrapper = shallowMount(Component, {
+		name: 'RecipeCard',
+		data() {
+			return {
+				recipe: {
+					name: '',
+					cook_time: 0,
+					img_url: '',
+					servings: 0,
+					summary: '',
+					health_score: 4,
+					weight_watcher_points: 2,
+					vegetarian: false,
+					vegan: false,
+					gluten_free: false,
+					dairy_free: false,
+					healthy: false,
+					sustainable: false,
+					directions: [],
+					ingredients: [],
+				},
+			};
+		},
+		mocks: {},
+		stubs: {},
+		store,
+		router,
+		localVue,
+	});
+});
+
+afterEach(() => {
+	wrapper.destroy();
+});
+
+describe('Close View Recipe', () => {
+	test('renders Settings with the default data', async () => {
+		await wrapper.vm.$nextTick();
+		wrapper.vm.close();
+		expect(wrapper.emitted('closeWindow')[0]).toEqual([true]);
+	});
+});
