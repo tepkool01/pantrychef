1a6e35c0925f0069758c3aa488f9472e4c47a88a
Michael Loredo <mjloredo@gmail.com>
2020-04-18 23:58:24 -0500

    adding api call mock to view recipe

--- front-end/tests/unit/ModalViewRecipe.spec.js
+++ front-end/tests/unit/ModalViewRecipe.spec.js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import { shallowMount, createLocalVue } from '@vue/test-utils';
 import Vuex from 'vuex';
 import { BootstrapVue } from 'bootstrap-vue';
@@ -5,16 +6,40 @@ import Component from '../../src/components/modals/ViewRecipe.vue';
 import store from '../../src/store';
 import router from '../../src/router';
 import api from '../../src/api';
-import axios from "axios";
 import profile from "../../src/store/modules/profile";
 
 let wrapper;
+
+jest.mock('axios');
 
 const localVue = createLocalVue();
 localVue.use(Vuex);
 localVue.use(BootstrapVue);
 
 beforeEach(() => {
+	axios.get.mockImplementation(() =>
+	Promise.resolve({
+		data: {
+			name: '',
+			cook_time: 0,
+			img_url: 'test',
+			servings: 0,
+			summary: '',
+			health_score: 4,
+			weight_watcher_points: 2,
+			vegetarian: false,
+			vegan: false,
+			gluten_free: false,
+			dairy_free: false,
+			healthy: false,
+			sustainable: false,
+			directions: [],
+			ingredients: [],
+		}
+	}
+	)
+	  );
+
 	wrapper = shallowMount(Component, {
 		name: 'RecipeCard',
 		data() {
@@ -52,6 +77,7 @@ afterEach(() => {
 
 describe('Close View Recipe', () => {
 	test('renders Settings with the default data', async () => {
+
 		await wrapper.vm.$nextTick();
 		wrapper.vm.close();
 		expect(wrapper.emitted('closeWindow')[0]).toEqual([true]);
