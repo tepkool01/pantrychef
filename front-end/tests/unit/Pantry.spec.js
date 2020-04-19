d59fc77b3998e6d58c104d549929dec7ea5d8351
Michael_Rodriguez <mmr94@txstate.edu>
2020-04-19 00:41:37 -0500

    Adding Pantry view test

--- front-end/tests/unit/Pantry.spec.js
+++ front-end/tests/unit/Pantry.spec.js
@@ -1,38 +1,78 @@
 import { shallowMount, createLocalVue } from '@vue/test-utils';
 import Vuex from 'vuex';
-import Pantry from '../../src/views/Pantry.vue';
+import { BootstrapVue } from 'bootstrap-vue';
+import Component from '../../src/views/Pantry.vue';
 import store from '../../src/store';
 import router from '../../src/router';
 
-import { EventBus } from '../../src/eventBus'; // used for Errors
-import Ingredient from '../../src/components/Ingredient.vue';
-import IngredientSubmissionPanel from '../../src/components/IngredientSubmissionPanel.vue';
-import { mapGetters, mapActions } from 'vuex';
-import Component from "../../src/components/ProfileCreate";
+let wrapper;
 
 const localVue = createLocalVue();
 localVue.use(Vuex);
+localVue.use(BootstrapVue);
 
-
-afterEach(() => {
-
+beforeEach(() => {
+	wrapper = shallowMount(Component, {
+		name: 'Pantry',
+		data() {
+			return {
+				sortOrder: 'default',
+				shoppingSortOrder: 'default',
+				ww_score: 25,
+			};
+		},
+		computed: {
+			orderedListOptions() {
+				return {
+					default: (list) => {
+						return list;
+					},
+				};
+			},
+		},
+		mocks: {},
+		stubs: {},
+		store,
+		router,
+		localVue,
+	});
 });
 
-describe('Pantry', () => {
-    test('renders Pantry view with expected props data', () => {
-        const wrapper = shallowMount(Pantry, {
-            propsData: {
-                pantryType: "shopping",
-                sortOrder: 'default',
-            },
-			store,
-			router,
-        });
+afterEach(() => {
+	wrapper.destroy();
+});
 
-        //expect(wrapper.vm.ingredient.ingredient_name).toMatch("test_ingredient");
-        //expect(wrapper.vm.listType).toMatch("shopping");
+describe('Component', () => {
+	test('renders pantry card for adding an ingredient from shopping list', async () => {
+		await wrapper.vm.$nextTick();
+		wrapper.vm.addIngredientToShoppingList('Butter');
+		expect(wrapper.emitted('title')[0]).toEqual(['Pantry']);
+	});
+});
 
-        wrapper.destroy();
-    });
+describe('Component', () => {
+	test('renders pantry card for removing an ingredient from shopping list', async () => {
+		await wrapper.vm.$nextTick();
+		wrapper.vm.removeIngredientFromShopping('Butter');
+		console.log(wrapper.emitted());
+		expect(wrapper.emitted('title')[0]).toEqual(['Pantry']);
+	});
+});
 
+describe('Component', () => {
+	test('renders pantry card for adding an ingredient to the pantry', async () => {
+		await wrapper.vm.$nextTick();
+		wrapper.vm.addIngredientToPantry('Butter');
+		console.log(wrapper.emitted());
+		expect(wrapper.emitted('title')[0]).toEqual(['Pantry']);
+	});
+});
+
+describe('Component', () => {
+	test('renders pantry card for removing an ingredient from the pantry', async () => {
+		await wrapper.vm.$nextTick();
+		wrapper.vm.handleIngredientRemove('Butter');
+		console.log(wrapper.emitted());
+		expect(wrapper.emitted('title')[0]).toEqual(['Pantry']);
+	});
 });
